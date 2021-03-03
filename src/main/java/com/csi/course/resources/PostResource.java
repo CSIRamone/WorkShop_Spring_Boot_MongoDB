924aa2f35f201b23054987d126653889a5715dc7
CSIRamone <cidinei_inacio@hotmail.com>
2021-03-03 14:18:59 -0300

    simple query with query methods

--- src/main/java/com/csi/course/resources/PostResource.java
+++ src/main/java/com/csi/course/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.csi.course.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.csi.course.domain.Post;
+import com.csi.course.resources.util.URL;
 import com.csi.course.services.PostService;
 
 @RestController
@@ -23,5 +27,11 @@ public class PostResource {
 		return ResponseEntity.ok().body(obj);		
 	}
 	
+	@RequestMapping(value="/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);		
+	}	
 		
 }
